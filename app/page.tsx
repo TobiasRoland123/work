453fe7a5c4d4348045387bf288c43d079c6c9725
Tobias Roland <tru@charlietango.dk>
2025-04-16 10:17:07 +0200

    feat: added colors as variables for taiwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import {Test} from "@/components/ui/Test/Test";
 
 
 
@@ -6,7 +7,7 @@ export default function Home() {
   return (
  <div>
    <Button variant="outline">Button</Button>
-
+<Test/>
  </div>
   );
 }
