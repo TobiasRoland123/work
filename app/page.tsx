28527558bb57dbd4dfef53be4ea2ea6385389964
Tobias Roland <tru@charlietango.dk>
2025-04-21 12:37:35 +0200

    added button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,3 @@
-import { Test } from '@/components/ui/Test/Test';
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
-  return (
-    <div>
-      <Button variant="default" className={'font-mono'}>
-        Button
-      </Button>
-
-      <Test />
-    </div>
-  );
+  return <div></div>;
 }
