4fb5b567f65638742c6a8b0c095f5d30ef49653b
Tobias Roland <tru@charlietango.dk>
2025-04-24 09:49:10 +0200

    removed static path

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -12,6 +12,6 @@ const config: StorybookConfig = {
     name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ['../public', '../public/fonts'],
+  staticDirs: ['../public'],
 };
 export default config;
