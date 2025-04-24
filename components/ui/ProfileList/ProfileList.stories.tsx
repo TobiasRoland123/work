795bf27ffe62a0a2e9b6b873830d95d09d6acf84
Snoopsie1 <oliverrasoli@gmail.com>
2025-04-24 15:24:53 +0200

    feat: profileList component + story

---
+++ components/ui/ProfileList/ProfileList.stories.tsx
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ProfileList } from './ProfileList';
+
+const meta: Meta<typeof ProfileList> = {
+  component: ProfileList,
+};
+
+export default meta;
+type Story = StoryObj<typeof ProfileList>;
+
+export const Default: Story = {
+  args: {
+    profiles: [
+      {
+        profilePicture: 'https://picsum.photos/200',
+        name: 'Anders Christensen',
+        title: 'UI Designer',
+        status: {
+          message: 'From home',
+        },
+        phoneNumber: '+45 87 18 91 28',
+        email: 'anders@work.com',
+      },
+      {
+        profilePicture: 'https://picsum.photos/200',
+        name: 'Anders Christensen',
+        title: 'UI Designer',
+        status: {
+          message: 'From home',
+        },
+        phoneNumber: '+45 87 18 91 28',
+        email: 'anders@work.com',
+      },
+      {
+        profilePicture: 'https://picsum.photos/200',
+        name: 'Anders Christensen',
+        title: 'UI Designer',
+        status: {
+          message: 'From home',
+        },
+        phoneNumber: '+45 87 18 91 28',
+        email: 'anders@work.com',
+      },
+      {
+        profilePicture: 'https://picsum.photos/200',
+        name: 'Anders Christensen',
+        title: 'UI Designer',
+        status: {
+          message: 'From home',
+        },
+        phoneNumber: '+45 87 18 91 28',
+        email: 'anders@work.com',
+      },
+      {
+        name: 'Anders Christensen',
+        title: 'UI Designer',
+        status: {
+          message: 'From home',
+        },
+        phoneNumber: '+45 87 18 91 28',
+        email: 'anders@work.com',
+      },
+    ],
+  },
+};
