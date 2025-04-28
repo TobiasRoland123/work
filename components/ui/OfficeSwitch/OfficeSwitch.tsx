e666cca04bfd664970859319c855d1c39ef2619e
Jachob Westh Møllegård <fili5863@stud.kea.dk>
2025-04-28 09:45:20 +0200

    fix:aria-label

--- components/ui/OfficeSwitch/OfficeSwitch.tsx
+++ components/ui/OfficeSwitch/OfficeSwitch.tsx
@@ -4,7 +4,10 @@ import { Switch } from '../switch';
 const OfficeSwitch = () => {
   return (
     <div>
-      <Switch className="data-[state=unchecked]:bg-light-blue data-[state=checked]:bg-blue h-7 w-12 [&>span]:size-6 [&>span]:data-[state=checked]:translate-x-[21px] [&>span]:data-[state=unchecked]:translate-x-[1px]" />
+      <Switch
+        aria-label="Toggle office mode"
+        className="data-[state=unchecked]:bg-light-blue data-[state=checked]:bg-blue h-7 w-12 [&>span]:size-6 [&>span]:data-[state=checked]:translate-x-[21px] [&>span]:data-[state=unchecked]:translate-x-[1px]"
+      />
     </div>
   );
 };
