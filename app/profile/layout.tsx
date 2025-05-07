b96ddff3b1b88b289822dcf5bcca3100820f02cb
Tobias Roland <tru@charlietango.dk>
2025-05-07 19:00:29 +0200

    added new layouts and moved links to separate file

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -1,20 +1,14 @@
 import { ReactNode } from 'react';
 import { SidebarProviderWrapper } from '@/components/SidebarProviderWrapper/SidebarProviderWrapper';
 import { MobileNavigation } from '@/components/ui/MobileNavigation/Mobilenavigation';
-import { linkProps } from '@/types/link';
-
+import { menuLinks } from '@/components/AppContent';
 export default function DashboardLayout({ children }: { children: ReactNode }) {
-  const links: Array<linkProps> = [
-    { href: '#', label: 'Today' },
-    { href: '#', label: 'Contact' },
-    { href: '#', label: 'Profile' },
-  ];
   return (
     <main>
       {/*This layout does not include the StatusSidebar*/}
-      <SidebarProviderWrapper links={links}>{children}</SidebarProviderWrapper>
+      <SidebarProviderWrapper links={menuLinks}>{children}</SidebarProviderWrapper>
 
-      <MobileNavigation linkList={links} />
+      <MobileNavigation linkList={menuLinks} />
     </main>
   );
 }
--- app/today/layout.tsx
+++ app/today/layout.tsx
@@ -1,21 +1,16 @@
 import { ReactNode } from 'react';
 import { SidebarProviderWrapper } from '@/components/SidebarProviderWrapper/SidebarProviderWrapper';
 import { MobileNavigation } from '@/components/ui/MobileNavigation/Mobilenavigation';
-import { linkProps } from '@/types/link';
+import { menuLinks } from '@/components/AppContent';
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
-  const links: Array<linkProps> = [
-    { href: '#', label: 'Today' },
-    { href: '#', label: 'Contact' },
-    { href: '#', label: 'Profile' },
-  ];
   return (
     <main>
-      <SidebarProviderWrapper links={links} showStatusSidebar>
+      <SidebarProviderWrapper links={menuLinks} showStatusSidebar>
         {children}
       </SidebarProviderWrapper>
 
-      <MobileNavigation linkList={links} />
+      <MobileNavigation linkList={menuLinks} />
     </main>
   );
 }
