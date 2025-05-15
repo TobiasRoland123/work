cb9a033ee0f7cc79bc9305a7adb10cf895e07b61
Snoopsie1 <oliverrasoli@gmail.com>
2025-05-15 08:45:31 +0200

    refactor: Added session validator to see if it isn't expired

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import { auth } from '@/auth';
 export default async function Home() {
   const session = await auth();
 
-  if (session?.user) {
+  const isValid = session?.user && (!session.expires || new Date(session.expires) > new Date());
+
+  if (isValid) {
     redirect('/today');
   } else {
     redirect('/login');
