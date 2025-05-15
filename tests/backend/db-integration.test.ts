3cef05da497473542644db17c1efbe26fa58f93d
Jachob Westh Møllegård <fili5863@stud.kea.dk>
2025-05-15 14:04:38 +0200

    fix pg og patter

--- tests/backend/db-integration.test.ts
+++ tests/backend/db-integration.test.ts
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   host: process.env.PGHOST,
-  port: Number(process.env.POSTGRES_PORT),
+  port: Number(process.env.PGPORT),
   database: process.env.PGDATABASE,
 });
 
