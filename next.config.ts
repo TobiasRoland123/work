52e19eb9145b377f1ce91a42e830c9c8af82d809
Jachob Westh Møllegård <fili5863@stud.kea.dk>
2025-05-20 10:54:48 +0200

    fix:added domain to config reseeded db

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos', 'hel1.your-objectstorage.com'],
+    domains: ['picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'hel1.your-objectstorage.com',
+        pathname: '/workbucket/profile-images/**',
+      },
+    ],
   },
 };
 
