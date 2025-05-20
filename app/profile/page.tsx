063967015d15da7219b0ad526a1ee9504c52776a
Jachob Westh Møllegård <fili5863@stud.kea.dk>
2025-05-20 09:53:48 +0200

    fix: removed hardcoded get user

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -13,7 +13,6 @@ const page = async () => {
   }
 
   const user = await userService.getUserById(session.userId);
-  // const user = await userService.getUserById('b0bb8dda-976d-4992-8922-4fef721c4b09');
 
   if (!user) {
     return <div>User not found.</div>;
