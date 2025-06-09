0e71ec66e12d61cddcd54d601eab6dd5f44cb36c
Tobias Roland <tru@charlietango.dk>
2025-06-09 11:25:05 +0200

    fix: added fe validateion to dates

--- components/Fields/DetailsField.tsx
+++ components/Fields/DetailsField.tsx
@@ -11,20 +11,22 @@ type DetailsFieldProps = {
 
 export function DetailsField({ form }: DetailsFieldProps) {
   const chosenStatus = form.watch('status');
-
+  return null;
   if (chosenStatus !== 'SICK' && chosenStatus !== undefined) {
     return (
       <FormField
         name={'detailsString'}
         control={form?.control}
         render={({ field }) => (
-          <FormItem className={'flex gap-11 justify-between items-end'}>
-            <FormLabel className={'min-w-[80px]'}>Details</FormLabel>
-            <FormControl className={'w-full'}>
-              <div className="flex flex-col gap-2">
-                <Input placeholder={getDetailsPlaceholder(form.watch('status'))} {...field} />
-              </div>
-            </FormControl>
+          <FormItem>
+            <div className={'flex gap-11 justify-between items-end'}>
+              <FormLabel className={'min-w-[80px]'}>Details</FormLabel>
+              <FormControl className={'w-full'}>
+                <div className="flex flex-col gap-2">
+                  <Input placeholder={getDetailsPlaceholder(form.watch('status'))} {...field} />
+                </div>
+              </FormControl>
+            </div>
             <FormMessage />
           </FormItem>
         )}
