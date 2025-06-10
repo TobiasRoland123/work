733178ac40e5b2b81bfa6705b33ef45dab35354a
Jachob Westh Møllegård <fili5863@stud.kea.dk>
2025-06-10 13:41:13 +0200

    fix:reverted production ternary

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase =
-  process.env.NODE_ENV !== 'production'
+  process.env.NODE_ENV === 'production'
     ? createClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
