68e223b3bf22b818147b0a8259680ba1933a88a7
Snoopsie1 <oliverrasoli@gmail.com>
2025-06-16 10:44:06 +0200

    fix: will now scroll to top when switching in/out office

--- app/today/layout.tsx
+++ app/today/layout.tsx
@@ -8,7 +8,7 @@ import Loading from './loading';
 export default async function DashboardLayout({ children }: { children: ReactNode }) {
   const session = await auth();
   return (
-    <main className="w-screen h-svh overflow-y-auto pl-[calc(100vw - 100%)]">
+    <main id="dashboard-main" className="w-screen h-svh overflow-y-auto pl-[calc(100vw - 100%)]">
       <SidebarProviderWrapper links={menuLinks} showStatusSidebar userId={session?.userId}>
         <Suspense fallback={<Loading />}>{children}</Suspense>
       </SidebarProviderWrapper>
--- components/ui/OfficeSwitch/OfficeSwitch.tsx
+++ components/ui/OfficeSwitch/OfficeSwitch.tsx
@@ -8,14 +8,12 @@ interface OfficeSwitchProps {
 
 const OfficeSwitch = ({ officeStatus, setOfficeStatus }: OfficeSwitchProps) => {
   return (
-    <div>
-      <Switch
-        checked={officeStatus}
-        onCheckedChange={setOfficeStatus}
-        aria-label="Toggle office mode"
-        className="data-[state=unchecked]:bg-light-blue data-[state=checked]:bg-blue h-7 w-12 [&>span]:size-6 [&>span]:data-[state=checked]:translate-x-[21px] [&>span]:data-[state=unchecked]:translate-x-[1px]"
-      />
-    </div>
+    <Switch
+      checked={officeStatus}
+      onCheckedChange={setOfficeStatus}
+      aria-label="Toggle office mode"
+      className="data-[state=unchecked]:bg-light-blue data-[state=checked]:bg-blue h-7 w-12 [&>span]:size-6 [&>span]:data-[state=checked]:translate-x-[21px] [&>span]:data-[state=unchecked]:translate-x-[1px]"
+    />
   );
 };
 
